055650b3e7582f5d68065c4a810fe345220d9feb
KartikPatil20 <kartikpatilcse@gmail.com>
2024-03-06 14:09:55 +0530

    Updating edge cases

--- src/App.js
+++ src/App.js
@@ -13,17 +13,22 @@ function App() {
       <h1>Full Name Display</h1>
       <form onSubmit={(e)=>{
         e.preventDefault();
-        setFullname(`${fname} ${lname}`);
+        if(fname === " " || fname === "" || lname === " " || lname === ""){
+          setFullname("");
+        }else{
+          setFullname(`${fname} ${lname}`);
+        }
+        
       }}>
       <div>
       <label>First Name:</label>
-      <input type='text' value={fname} onChange={(e)=>setFname(e.target.value)}></input>
+      <input type="text" value={fname} onChange={(e)=>setFname(e.target.value)}></input>
       </div>
       <div>
       <label>Last Name:</label>
-      <input type='text' value={lname} onChange={(e)=>setLname(e.target.value)}></input>
+      <input type="text" value={lname} onChange={(e)=>setLname(e.target.value)}></input>
       </div>
-      <button >Submit</button>
+      <button type="submit" >Submit</button>
       </form>
       {fullname && <h2>Full Name: {fullname}</h2>}
     </div>
